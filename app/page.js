674f8fae1f161a661c5541fe4db27b9772deef88
eveyiyi <yiyi.zhao@edu.sait.ca>
2024-09-24 13:11:10 -0600

    Week-2 Assignment
    Create StudentInfo Component, Edit Root Page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
-   
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="./week-2/">Go to Week 2 Page</Link>
+      </p>
+    </div>
+
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Name: Eve Zhao</h1>
+      <p>
+        <a href="https://github.com/eveyiyi">https://github.com/eveyiyi</a>
+      </p>
+    </div>
+  );
+};
+
+
